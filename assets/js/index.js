13578762b0e574b63c0f8af7f49244233e129a33
SwetaDhanvantri <dhanvantrisweta@gmail.com>
2022-10-29 09:31:52 +0530

    save changes all

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,50 @@
-/* big slider */
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
+ /* big slider */
+ let slideIndex = 0;
+ showSlides();
+ 
+ function showSlides() {
+   let i;
+   let slides = document.getElementsByClassName("mySlides");
+   let dots = document.getElementsByClassName("dot");
+   for (i = 0; i < slides.length; i++) {
+     slides[i].style.display = "none";  
+   }
+   slideIndex++;
+   if (slideIndex > slides.length) {slideIndex = 1}    
+   for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" active", "");
+   }
+   slides[slideIndex-1].style.display = "block";  
+   dots[slideIndex-1].className += " active";
+   setTimeout(showSlides, 6000); // Change image every 2 seconds
+ }
+ 
+ /* big slider end */
+ /*slide review */
+ let reviewSlideIndex = 0;
+ reviewSlides();
+ function reviewSlides() {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+  let rSlides =document.getElementsByClassName("slide-review");
+  let reviewDots = document.getElementsByClassName("r-dot");
+  for (i = 0; i <  rSlides.length; i++){
+    rSlides[i].style.display ="none";
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  reviewSlideIndex++;
+  if(reviewSlideIndex > rSlides.length){
+    reviewSlideIndex = 1;
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
+  for( i = 0; i < reviewDots.length; i++){
+    reviewDots[i].className = reviewDots[i].className.replace("active", " ");
+  }
+  rSlides[reviewSlideIndex-1].style.display= "block";
+  reviewDots[reviewSlideIndex-1].className += " active";
+  setTimeout(reviewSlides, 2000);
+ }
 
-/* big slider end */
+ /* slide review end */
+ /* Range slider */
+ $('#rangeval').mdbRange({
+    
+});
+/* range slider end */
